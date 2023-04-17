65845c4596fefcaefe69a83a9fe9c7c386dcb35d
adiy55 <adiyafe55@gmail.com>
2023-04-18 00:07:51 +0300

    Add background + scrips + create car spawners

---
+++ Assets/Scripts/DestroyOnTrigger2D.cs
@@ -0,0 +1,22 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class DestroyOnTrigger2D : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == triggeringTag && enabled) {
+            Debug.Log("Detroy!");
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update() {
+        /* Just to show the enabled checkbox in Editor */
+    }
+}
